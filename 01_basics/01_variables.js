7b398ebb00e1a4de5d55e0ad6be33f3212717d41
vinita2992 <79807434+vinita2992@users.noreply.github.com>
2025-06-13 10:57:46 +0000

    data type & variable names

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 4;
+let accountEmail = "Vinitanagpure@gmail.com";
+var accountPassword = 345678 ;
+accountCity = "Indore"; 
+
+let accountState
+
+// accountId = 3 ; not Allowd
+
+accountEmail= "vini@gmail.com";
+accountPassword = '123456';
+accountCity = "Jaipur";
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "John";
+let age = 30;
+let isEmployed = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+console.log(typeof name); // string
+console.log(typeof null); // object (this is a known JavaScript quirk)
+console.log(typeof undefined); 
+
+//number = 2 the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+//symbol// object => { key: value }
+// null => empty value (standlone value)
+// undefined => variable not defined
+
+//objects ---- Non-primitive data types
