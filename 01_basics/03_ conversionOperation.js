75bfae8d9e84d6fba68bfe688f14cda1b2d62084
vinita2992 <79807434+vinita2992@users.noreply.github.com>
2025-06-13 11:39:36 +0000

    conversion-file

---
+++ 01_basics/03_ conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33 ;
+
+
+console.log(typeof + score);
+console.log(typeof + (score));
+
+
+ let valueInNumber = Number(score);
+
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+ //"33" = 33
+ // "33abcd" = NaN
+ //true = 1; false = 0;
+
+let valueInNumber2 = Number("33abcd");
+console.log(typeof valueInNumber2);
+
+let isLoggedIn = "vinita";
+
+let isLoggedInNumber = Boolean(isLoggedIn);
+
+let someNumber = 55;
+
+let someNumberString = String(someNumber);
+
+console.log(typeof someNumberString);
+console.log(typeof someNumber);
